2ae8850f0d67819cddc455812bd279a8a194d87d
sota1235 <sota1235@gmail.com>
2023-08-21 10:13:11 +0900

    modify: fix for lint

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import logger from 'morgan';
 import { setRoutes } from './routes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 // TODO switch format for Prod
 app.use(logger('dev'));
